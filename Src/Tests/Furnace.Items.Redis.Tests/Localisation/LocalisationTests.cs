ae34a047b09b69e69141fdec8def478dc928438d
Aaron Job <awjob42@gmail.com>
2014-05-08 21:25:13 +0100

    Clened up formatting and names

--- Src/Tests/Furnace.Items.Redis.Tests/Localisation/LocalisationTests.cs
+++ Src/Tests/Furnace.Items.Redis.Tests/Localisation/LocalisationTests.cs
@@ -8,38 +8,73 @@ namespace Furnace.Items.Redis.Tests.Localisation
     public abstract class LocalisationTests : RedisBackedFurnaceItemsTests
     {
         protected CultureInfo CultureInfo;
+        protected Stub CultureStub;
+        protected Stub DefultCultureStub;
+        protected long Id;
+        protected string Key;
 
         protected LocalisationTests(string furnaceItemsType) : base(furnaceItemsType)
         {
+        }
+
+        [SetUp]
+        public void LocalisationTestssetUp()
+        {
+            Id = 99L;
+            Key = RedisBackedFurnaceItems.CreateItemKey(Id, typeof(Stub));
+            DefultCultureStub = new Stub {Test = "Hello"};
+            Client.Hashes[Key][SiteConfiguration.DefaultSiteCulture.Name].Returns(DefultCultureStub.BuildSerialisedString());
         }
 
         public class GivenJapaneseAsTheCulture : LocalisationTests
         {
             public GivenJapaneseAsTheCulture(string furnaceItemsType) : base(furnaceItemsType)
             {
-                //Assign
-                var culture = "ja-JP";
-                CultureInfo = CultureInfo.GetCultureInfo(culture);
             }
 
-            [Test]
-            public void WhenGetItemIsCalledThenTheReturnedItem_IsCorrect()
+            [SetUp]
+            public void GivenJapaneseAsTheCultureSetUp()
             {
-                const long id = 1L;
-
-                const string propityValue = "こんにいちわ";
-                var returnJon = new Stub { Test = propityValue }.BuildSerialisedString();
-
-                var key = RedisBackedFurnaceItems.CreateItemKey(id, typeof(Stub));
-
-                Client.Hashes[key][CultureInfo.Name].Returns(returnJon);
-
-                //Act
-                var result = Sut.GetItem<Stub>(id, CultureInfo);
-
-                //Assert
-                Assert.That(result.Test, Is.EqualTo(propityValue));
+                CultureInfo = CultureInfo.GetCultureInfo("fr-FR");
+                CultureStub = new Stub { Test = "こんにいちわ" };
             }
+        }
+
+        public class GivenFrenchAsTheCulture : LocalisationTests
+        {
+            public GivenFrenchAsTheCulture(string furnaceItemsType) : base(furnaceItemsType)
+            {
+            }
+
+            [SetUp]
+            public void GivenJapaneseAsTheCultureSetUp()
+            {
+                CultureInfo = CultureInfo.GetCultureInfo("ja-JP");
+                CultureStub = new Stub { Test = "bonjour" };
+            }
+        }
+
+        [Test]
+        public void WhenGetItemWithCultureInfoIsCalled_ThenTheReturnedItem_IsCorrect()
+        {
+            //Assign
+            Client.Hashes[Key][CultureInfo.Name].Returns(CultureStub.BuildSerialisedString());
+
+            //Act
+            var result = Sut.GetItem<Stub>(Id, CultureInfo);
+
+            //Assert
+            Assert.That(result.Test, Is.EqualTo(CultureStub.Test));
+        }
+
+        [Test]
+        public void WhenGetItemIsCalled_ThenTheReturnedItem_IsDefaultCulture()
+        {
+            //Act
+            var result = Sut.GetItem<Stub>(Id);
+
+            //Assert
+            Assert.That(result.Test, Is.EqualTo(DefultCultureStub.Test));
         }
     }
 }
